5b649bc4a1b57c95148eeeefa6702e6d8f8848af
nastyatsiahlik <tyahliki.n@gmail.com>
2022-11-20 21:11:42 +0300

    aaaaaaaaa

--- src/test/java/ZaraTest.java
+++ src/test/java/ZaraTest.java
@@ -17,7 +17,7 @@ public class ZaraTest {
 
     @BeforeTest
     public void browserSetup(){
-        System.setProperty("webdriver.chrome.driver", "D:\\chromdriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
         driver.get("https://www.zara.com/by/ru/");
     }
 
